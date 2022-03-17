9cb308979c72b39d2d36fdae72bac8fd54b96ff9
Chris Waters <floridarealty001@gmail.com>
2022-03-17 14:09:54 -0400

    finished

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@
 const Post = require('./Post');
 const User = require('./User');
 const Vote = require('./Vote');
+const Comment = require('./Comments');
 
 // create associations
 User.hasMany(Post, {
@@ -41,6 +42,21 @@ Post.hasMany(Vote, {
   foreignKey: 'post_id'
 });
 
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
 
 
-module.exports = { User, Post, Vote };
+module.exports = { User, Post, Vote, Comment };
